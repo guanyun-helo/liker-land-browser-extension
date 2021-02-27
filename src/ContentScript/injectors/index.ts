edc27d6359a957b1b974f5aa72cf384c109bf885
guanyun-helo <guanyun.helo@gmail.com>
2021-02-27 11:11:50 +0800

    fix: delete checklikerid method

--- src/ContentScript/injectors/index.ts
+++ src/ContentScript/injectors/index.ts
@@ -4,23 +4,17 @@ import YoutubePlugin from './youtube.plugin';
 
 class Injector {
   public static injectAll() {
-    console.log('=>>1');
     if (location.hostname === 'www.youtube.com') {
-      console.log('=>>2');
       if (document.querySelector('.button-container')) {
-        console.log('=>>3');
         const ele = document.querySelector('.button-container') as HTMLElement;
         if (!ele.parentElement) return;
-        // ele.parentElement.removeChild(ele);
       }
       // init button in next loop;
       setTimeout(() => {
         const youtubePlugin = new YoutubePlugin();
         youtubePlugin.inject();
-      }, 1000);
+      }, 0);
     }
-
-    // inject youtube plugin
   }
 }
 
